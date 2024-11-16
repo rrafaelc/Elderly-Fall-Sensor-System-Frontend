a1602c11a212a1a2a30fb21a2352b02f10897dce
Rafael Costa <rafaelcosta8020@gmail.com>
2024-11-16 17:20:37 -0300

    Imprimir dados do idoso

--- src/pages/Sensor/IdosoDescricoes.tsx
+++ src/pages/Sensor/IdosoDescricoes.tsx
@@ -1,11 +1,12 @@
-import { Card, Avatar } from "antd";
+import { Card, Avatar, Button } from "antd";
 import axios from "axios";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { IUser } from "modules/auth/types";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { toast } from "react-toastify";
 import { PatternFormat } from "react-number-format";
+import { useReactToPrint } from "react-to-print";
 
 export interface Idoso {
   id: number;
@@ -21,14 +22,20 @@ export interface Idoso {
   conditions?: string;
   name: string;
   date_of_birth: string;
+  blood_type: string;
   created_at: string;
   updated_at: string;
 }
 
 export const IdosoDescricoes = () => {
   const host = import.meta.env.VITE_API_HOST;
-  const [loading, setLoading] = useState<boolean>(true);
+  const [, setLoading] = useState<boolean>(true);
   const [idoso, setIdoso] = useState<Idoso | null>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const reactToPrintFn = useReactToPrint({
+    contentRef,
+    documentTitle: "Dados do idoso",
+  });
 
   const token = localStorage.getItem("token");
   const user = JSON.parse(localStorage.getItem("user")!) as IUser;
@@ -76,45 +83,65 @@ export const IdosoDescricoes = () => {
   };
 
   return (
-    <div className="w-full flex justify-center sm:justify-normal">
-      <Card loading={false} className="w-full max-w-[300px] sm:max-w-none">
-        <Card.Meta
-          className="w-full"
-          avatar={<Avatar src="images/elder.png" />}
-          title="Dados do idoso"
-          description={
-            idoso ? (
-              <div className="flex flex-col sm:gap-1">
-                <p>Nome: {idoso.name}</p>
-                <p>RG: {idoso.rg}</p>
-                <p>
-                  CPF:{" "}
-                  <PatternFormat
-                    format="###.###.###-##"
-                    displayType="text"
-                    value={idoso.cpf}
-                  />
-                </p>
-                <p>
-                  Data de Nascimento: {formatBirthDate(idoso.date_of_birth)}
-                </p>
-                <p>
-                  Endereço: {idoso.street}, {idoso.street_number}
-                </p>
-                <p>Bairro: {idoso.neighborhood}</p>
-                <p>Cidade: {idoso.city}</p>
-                <p>Estado: {idoso.state}</p>
-                <p>CEP: {idoso.zip_code}</p>
-                <p>Condições: {idoso.conditions ?? "N/A"}</p>
-                <p>Criado em: {formatDate(idoso.created_at)}</p>
-                <p>Atualizado em: {formatDate(idoso.updated_at)}</p>
-              </div>
-            ) : (
-              <p>Carregando...</p>
-            )
-          }
-        />
-      </Card>
+    <div className="w-full flex flex-col">
+      <div
+        ref={contentRef}
+        className="flex flex-col w-full items-center sm:flex-1"
+      >
+        <Card
+          loading={false}
+          className="h-full w-full max-w-[300px] sm:max-w-none"
+        >
+          <Card.Meta
+            className="w-full"
+            avatar={<Avatar src="images/elder.png" />}
+            title="Dados do idoso"
+            description={
+              idoso ? (
+                <div className="flex flex-col sm:gap-1">
+                  <p>Nome: {idoso.name}</p>
+                  <p>RG: {idoso.rg}</p>
+                  <p>
+                    CPF:{" "}
+                    <PatternFormat
+                      format="###.###.###-##"
+                      displayType="text"
+                      value={idoso.cpf}
+                    />
+                  </p>
+                  <p>
+                    Data de Nascimento: {formatBirthDate(idoso.date_of_birth)}
+                  </p>
+                  <p>
+                    Tipo sanguíneo: {idoso.blood_type}
+                  </p>
+                  <p>
+                    Endereço: {idoso.street}, {idoso.street_number}
+                  </p>
+                  <p>Bairro: {idoso.neighborhood}</p>
+                  <p>Cidade: {idoso.city}</p>
+                  <p>Estado: {idoso.state}</p>
+                  <p>CEP: {idoso.zip_code}</p>
+                  <p>Condições: {idoso.conditions ?? "N/A"}</p>
+                  <p>Criado em: {formatDate(idoso.created_at)}</p>
+                  <p>Atualizado em: {formatDate(idoso.updated_at)}</p>
+                </div>
+              ) : (
+                <p>Carregando...</p>
+              )
+            }
+          />
+        </Card>
+      </div>
+      <div className="flex justify-center mx-auto bg-white w-full max-w-[300px] sm:max-w-none">
+        <Button
+          type="primary"
+          onClick={() => reactToPrintFn()}
+          className="my-2"
+        >
+          Imprimir
+        </Button>
+      </div>
     </div>
   );
 };
